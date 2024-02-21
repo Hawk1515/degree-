1e1907fa8aee2559717ccccafcfa3bbe3132b87c
Farrukh <farrukhataev@gmail.com>
2024-02-21 18:06:48 +0500

    sikl 1-dars

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("1-uy ishi")
+let a = + prompt("son kiritng biz uni juft yoki toqligini aniqlaymiz")
+while (isNaN(a) || a == 0) {
+    a = + prompt("nmadir notogri " + " iltimos son kirting")
+}
+if (a % 2 == 0) {
+    alert("juft ")
+} else if (a % 2 > 0) {
+    alert("toq")
+}
+
+alert("2-uy ishi")
+// let d = +prompt("sonnini kiriting")
+// let b = +prompt("darajani kiriting")
+// let f = 1
+// for (let i = 0; i < b; i++) {
+//     f = d
+// }
+// alert(f);
+
